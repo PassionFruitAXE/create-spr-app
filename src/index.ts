7571cd06bb00bdc9e6d00efa77f8b3219f966a66
罗巍 <86441933@qq.com>
2023-05-06 22:19:11 +0800

    feat: 初步指定交互流程

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,74 @@
+import createPackageJson from "./lib/package";
+import inquirer from "inquirer";
+import path from "path";
+import { createDependenceByTemplate } from "./lib/dependence";
+import { execa } from "execa";
 import { program } from "commander";
 
-program.version("1.0.0");
+function init() {
+  program.version("1.0.0");
+  program
+    .command("start")
+    .description("启动cli")
+    .action(async () => {
+      /** write code here */
+      console.log("cli start");
+      /** 创建package.json对象 */
+      const packageJson = createPackageJson();
+      /** 获取用户选择包管理器 模板 */
+      const { projectName, packageManager, template } = await inquirer.prompt([
+        {
+          type: "input",
+          name: "projectName",
+          message: "输入项目名称",
+        },
+        {
+          type: "list",
+          name: "packageManager",
+          message: "选择包管理器",
+          choices: ["pnpm", "yarn", "npm"],
+        },
+        {
+          type: "list",
+          name: "template",
+          message: "选择模板",
+          choices: ["react"],
+        },
+      ]);
+      /** 获取用户选择打包工具 默认依赖 是否需要GitHub Action */
+      const { builder, dependence, isGithubActionRequired } =
+        await inquirer.prompt([
+          {
+            type: "list",
+            name: "builder",
+            message: "选择打包工具",
+            choices: ["vite"],
+          },
+          {
+            type: "checkbox",
+            name: "dependence",
+            message: "默认依赖包",
+            choices: createDependenceByTemplate(template),
+          },
+          {
+            type: "confirm",
+            name: "isGithubActionRequired",
+            message: "是否需要Github Action",
+          },
+        ]);
+      /** 创建package.json文件 */
+      packageJson.generateFile("");
+      // const projectPath = "";
+      // /** 安装依赖 */
+      // execa(`${packageManager} install`, [], {
+      //   cwd: path.join(process.cwd(), projectPath),
+      //   stdio: ["inherit", "pipe", "inherit"],
+      // });
 
-program
-  .command("start")
-  .description("start cli")
-  .action(() => {
-    console.log("cli starting");
-  });
+      console.log("cli end");
+    });
 
-program.parse(process.argv);
+  program.parse(process.argv);
+}
+
+init();
--- src/lib/package.ts
+++ src/lib/package.ts
@@ -1,3 +1,17 @@
-export default class PackageJson {
+class PackageJson {
   constructor() {}
+
+  /**
+   * 生成package.json文件
+   * @param path 生成路径
+   */
+  generateFile(path: string) {}
+}
+
+/**
+ * PackageJson工厂
+ * @returns PackageJson实例
+ */
+export default function createPackageJson() {
+  return new PackageJson();
 }
